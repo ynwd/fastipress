b20ee15d83e6110466f372e8558e99ac073ee28b
ynwd <ynwd@users.noreply.github.com>
2019-01-25 00:51:20 +0700

    Update

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 
-const FIREBASE_CLIENT_OPTIONS = [ 
-  'FIREBASE_API_KEY', 
-  'FIREBASE_AUTH_DOMAIN', 
-  'FIREBASE_DATABASE_URL' , 
-  'FIREBASE_STORAGE_BUCKET', 
-  'FIREBASE_MESSAGING_SENDER_ID' 
+const FIREBASE_CLIENT_OPTIONS = [
+  'FIREBASE_API_KEY',
+  'FIREBASE_AUTH_DOMAIN',
+  'FIREBASE_DATABASE_URL',
+  'FIREBASE_STORAGE_BUCKET',
+  'FIREBASE_MESSAGING_SENDER_ID'
 ]
 
 const schema = {
@@ -12,11 +12,11 @@ const schema = {
   required: [ 'PORT', ...FIREBASE_CLIENT_OPTIONS ],
   properties: {
     PORT: { type: 'string', default: 3000 },
-    FIREBASE_API_KEY: { type: 'string'},
-    FIREBASE_AUTH_DOMAIN: { type: 'string'},
-    FIREBASE_DATABASE_URL: { type: 'string'},
-    FIREBASE_STORAGE_BUCKET: { type: 'string'},
-    FIREBASE_MESSAGING_SENDER_ID: { type: 'string'}
+    FIREBASE_API_KEY: { type: 'string' },
+    FIREBASE_AUTH_DOMAIN: { type: 'string' },
+    FIREBASE_DATABASE_URL: { type: 'string' },
+    FIREBASE_STORAGE_BUCKET: { type: 'string' },
+    FIREBASE_MESSAGING_SENDER_ID: { type: 'string' }
   }
 }
 
--- plugins/support.js
+++ plugins/support.js
@@ -3,9 +3,17 @@
 const fp = require('fastify-plugin')
 
 async function superPlugin (fastify, opts) {
-  fastify.decorate('someSupport', function () {
-    return 'hugs'
-  })
+  fastify
+    .decorate('someSupport', function () {
+      return 'hugs'
+    })
+    .decorate('authPreHandler', async function auth (request, reply) {
+      // try {
+      //   await fastify.admin.auth().verifyIdToken(idToken)
+      // } catch (err) {
+      //   reply.send(err)
+      // }
+    })
 }
 
 module.exports = fp(superPlugin, {
